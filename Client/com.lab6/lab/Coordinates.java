49a1cd93b7b1ceb1239626f3dbb35a33d57bc7e2
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2021-06-08 04:11:47 +0300

    Add files via upload

---
+++ Client/com.lab6/lab/Coordinates.java
@@ -0,0 +1,45 @@
+package com.lab6.lab;
+
+import com.lab6.parameters.numericalParameters.FloatParameter;
+import com.lab6.parameters.numericalParameters.IntegerParameter;
+
+import java.io.Serializable;
+
+public class Coordinates implements Serializable {
+    private static final long serialVersionUID = 554565365L;
+    private Float x; //Поле не может быть null
+    private Integer y; //Максимальное значение поля: 139, Поле не может быть null
+    public static Coordinates DEFAULT = new Coordinates(0f, 0);
+
+    public static class Params {
+        public static FloatParameter x = new FloatParameter(0f);
+        public static IntegerParameter y = new IntegerParameter(0).setLowerBound(-314);
+    }
+
+    public Coordinates(Float x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinates() {
+    }
+
+    public Float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+
+}
+
---
+++ Client/com.lab6/lab/Status.java
@@ -0,0 +1,10 @@
+package com.lab6.lab;
+
+public enum Status {
+    NONE,
+    FIRED,
+    HIRED,
+    RECOMMENDED_FOR_PROMOTION,
+    REGULAR,
+    PROBATION
+}
