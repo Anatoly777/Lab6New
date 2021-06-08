6a2496b29a37e59878e347db9a53af2e67150ac4
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2021-06-08 04:10:59 +0300

    Add files via upload

---
+++ Client/com.lab6/io/OutputManager.java
@@ -0,0 +1,6 @@
+package com.lab6.io;
+
+public abstract class OutputManager {
+    public abstract void print(String s);
+    public abstract void println(String s);
+}
---
+++ Client/com.lab6/io/VoidOutputManager.java
@@ -0,0 +1,6 @@
+package com.lab6.io;
+
+public class VoidOutputManager extends OutputManager{
+    public void print(String s){}
+    public void println(String s){}
+        }
